ff1ea50c653ac9716dba7f6a95bf1e71ec1765f2
SuyogDhote <dhotesuyog4@gmail.com>
2024-07-05 15:53:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+const wordList = [
+    {
+        word: 'idea',
+        hint: 'a thought or suggestion'
+    },
+
+    {
+        word: 'search',
+        hint: 'act to find something'
+    },
+
+    {
+        word: 'meow',
+        hint: 'a cat does...'
+    },
+
+    {
+        word: 'gaming',
+        hint: 'gamers love ...'
+    },
+
+    {
+        word: 'jpeg',
+        hint: 'a small image file format'
+    },
+
+    {
+        word: 'mario',
+        hint: 'a famous game character with initial letter M'
+    },
+
+    {
+        word: 'mall',
+        hint: 'huge building with many shops'
+    },
+
+    {
+        word: 'vegeta',
+        hint: 'the face of pride among saiyans'
+    },
+
+    {
+        word: 'music',
+        hint: 'something everyone listens to...'
+    },
+
+    {
+        word: 'word',
+        hint: 'what you are guessing right now'
+    },
+
+    {
+        word: 'paint',
+        hint: 'used to color houses'
+    },
+
+    {
+        word: 'diwali',
+        hint: 'festival of light'
+    },
+
+    {
+        word: 'holi',
+        hint: 'festival of colors'
+    },
+
+    {
+        word: 'brain',
+        hint: 'something you used for thinking the answers'
+    },
+
+    {
+        word: 'calm',
+        hint: 'we should stay ... in every situation'
+    },
+
+    {
+        word: 'ghosts',
+        hint: 'something otherworldy mostly everyone is scared of'
+    },
+
+    {
+        word: 'god',
+        hint: 'someone who we have never seen but gives us hope'
+    },
+
+    {
+        word: 'alarm',
+        hint: 'it helps you wake up in the morning'
+    },
+
+    {
+        word: 'tea',
+        hint: 'drink without which the morning is incomplete'
+    },
+
+    {
+        word: 'lion',
+        hint: 'king of the jungle'
+    },
+
+    {
+        word: 'cat',
+        hint: `dog's dislike them`
+    },
+    
+    {
+        word: 'icecream',
+        hint: 'a cold dessert that you like'
+    },
+
+    {
+        word: 'jungle',
+        hint: 'where wild animals live'
+    },
+
+    {
+        word: 'apple',
+        hint: 'a ... a day keeps the doctor away'
+    },
+
+    {
+        word: 'health',
+        hint: 'Most Important Wealth'
+    },
+
+    {
+        word: 'dog',
+        hint: `Human's Most Loyal BestFriend`
+    }
+
+]
